65ebc514abe8df61087565b78863a21bb95dcec5
Tiago <tiago.rodrigues.simoes@gmail.com>
2017-11-20 17:42:52 +0000

    3.11 Showing CreatedOn Date

--- app.js
+++ app.js
@@ -27,10 +27,11 @@
 	app.controller("ReviewController", function(){
 		this.review = {};
 
-		this.addReview = function(product){
-			product.reviews.push(this.review);
-			this.review={};
-		};
+        this.addReview = function(product){
+            this.review.createdOn = Date.now();
+            product.reviews.push(this.review);
+            this.review = {};
+        };
 	});
 
 	var gems = [{
