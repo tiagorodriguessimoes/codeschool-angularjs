3b044e405e795d834f1636984db434fefcf3df73
Tiago <tiago.rodrigues.simoes@gmail.com>
2017-11-20 13:47:26 +0000

    2.6 Tabs Inside Out

--- app.js
+++ app.js
@@ -5,25 +5,36 @@
 		this.products = gems;
 	});
 
-	var gems = [
-        {
-    		name: 'Dodecahedron',
-    		price: 2.95,
-    		description: '. . .',
-            canPurchase: true,
-            soldOut: false,
-            images: [
-                    // './images/dodecahedron/dodecahedron-01-full.jpg',
-                    // './images/dodecahedron/dodecahedron-01-thumb.jpg'
-            ]
-    	},
-        {
-            name: 'Pentagonal Gem',
-    		price: 5.95,
-    		description: '. . .',
-            canPurchase: true,
-            soldOut: false,
-        }
-    ]
+	app.controller("PanelController", function() {
+		this.tab = 1;
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+	})
+
+	var gems = [{
+			name: 'Dodecahedron',
+			price: 2.95,
+			description: '. . .',
+			canPurchase: true,
+			soldOut: false,
+			images: [{
+				full: './images/dodecahedron/dodecahedron-01-full.jpg',
+				thumb: './images/dodecahedron/dodecahedron-01-thumb.jpg'
+			}]
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: 5.95,
+			description: '. . .',
+			canPurchase: true,
+			soldOut: false,
+		}
+	]
 
 })();
