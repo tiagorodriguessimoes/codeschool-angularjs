3d99b8a45b796611806f351ad1bb6eb403ba1c32
Tiago <tiago.rodrigues.simoes@gmail.com>
2017-11-21 11:51:33 +0000

    4.7 Refactoring Product Gallery

--- app.js
+++ app.js
@@ -5,11 +5,18 @@
 		this.products = gems;
 	});
 
-	app.controller("GalleryController", function(){
-        this.current = 0;
-        this.setCurrent = function(newGallery){
-            this.current = newGallery || 0;
-        };
+    app.directive('productGallery', function(){
+      return{
+        restrict: 'E',
+        templateUrl: "product-gallery.html",
+        controller: function(){
+          this.current = 0;
+          this.setCurrent = function(imageNumber){
+            this.current = imageNumber || 0;
+          };
+        },
+        controllerAs: 'gallery',
+      };
     });
 
 	app.controller("ReviewController", function(){
